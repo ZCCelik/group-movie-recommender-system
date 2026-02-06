73ec5a51d73c1947b02d7232105ca410957c9008
ZCCelik <117162677+ZCCelik@users.noreply.github.com>
2026-02-06 13:33:23 +0300

    use sqlalchemy 2.0 for tables and make movie_genres association table

--- backend/app/models/movie.py
+++ backend/app/models/movie.py
@@ -1,19 +1,37 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Float, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, Float, Table, ForeignKey, Date
+from app.db.base import Base
 
-Base = declarative_base()
+movie_genres = Table(
+    "movie_genres",
+    Base.metadata,
+    mapped_column(
+        "movie_id",
+        ForeignKey("movies.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    mapped_column(
+        "genre_id",
+        ForeignKey("genres.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+)
 
 class Movie(Base):
     __tablename__ = "movies"
 
-    id = Column(Integer, primary_key=True, index=True)
-    tmdb_id = Column(Integer, unique=True, index=True)
-    title = Column(String, index=True)
-    overview = Column(Text)
-    release_date = Column(String)
-    language = Column(String)
-    genres = Column(String)
-    popularity = Column(Float)
-    vote_average = Column(Float)
-    vote_count = Column(Integer)
-    poster_path = Column(String)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    tmdb_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False, index=True)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    overview: Mapped[str | None] = mapped_column(String, nullable=True)
+    release_date: Mapped[Date | None] = mapped_column(Date, nullable=True)
+    language: Mapped[str | None] = mapped_column(String, nullable=True)
+    popularity: Mapped[float | None] = mapped_column(Float, nullable=True)
+    vote_average: Mapped[float | None] = mapped_column(Float, nullable=True)
+    vote_count: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    poster_path: Mapped[str | None] = mapped_column(String, nullable=True)
+
+    genres: Mapped[list["Genre"]] = relationship(
+        secondary="movie_genres",
+        back_populates="movies"
+    )
